51a5b3a55f5aa1535f950f87970df6e62d3329c0
florian <flodal974@gmail.com>
2023-08-16 18:37:51 +0200

    ajout avis

--- index.js
+++ index.js
@@ -1,43 +1,22 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+function afficherAvis() {
+    const nom = document.getElementById('nom').value;
+    const avis = document.getElementById('avis').value;
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB69-DLI9BAU5gy0PDH8BiaDYa4reVri18",
-  authDomain: "garage-v-parrot-ecf.firebaseapp.com",
-  projectId: "garage-v-parrot-ecf",
-  storageBucket: "garage-v-parrot-ecf.appspot.com",
-  messagingSenderId: "156806295937",
-  appId: "1:156806295937:web:0343d5182fbb5305a5b880",
-  measurementId: "G-2M6PPTNB5Y"
-};
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
+    messageElement.textContent = `${nom}: ${avis}`;
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-/***************************** */
-
-
-/**************************** */
-
-const loginForm = document.getElementById('login-form');
-
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const identifiant = loginForm['identifiant'].value;
-    const password = loginForm['password'].value;
-
-    firebase.auth().signInWithEmailAndPassword(name, password)
-        .then((userCredential) => {
-            // Connexion réussie, rediriger ou effectuer des actions nécessaires
-            window.location.href = "tableau de bord.html";
-        })
-        .catch((error) => {
-            // Gérer les erreurs d'authentification
-            console.error('Erreur de connexion:', error);
-        });
-});
+    const container = document.querySelector('.container-messages');
     
+    // Insérer le message en tant que premier enfant (au début de la liste)
+    container.insertBefore(messageElement, container.firstChild);
 
+    // Réinitialisation des champs d'entrée
+    document.getElementById('nom').value = '';
+    document.getElementById('avis').value = '';
+}
 
+document.addEventListener('DOMContentLoaded', function () {
+    const publierBtn = document.getElementById('publier-btn');
+    publierBtn.addEventListener('click', afficherAvis);
+});
